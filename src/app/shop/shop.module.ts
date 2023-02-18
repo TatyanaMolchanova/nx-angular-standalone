f5d73f5a512461ca1c08b9cd3090a2649da11997
Tatyana Molchanova <mtguspeh@ukr.net>
2023-02-18 21:53:34 +0200

    initial app.module.ts is absent again

---
+++ src/app/shop/shop.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShopComponent } from './shop.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ShopComponent]
+})
+export class ShopModule { }
